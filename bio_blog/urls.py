9da41ee908a8acc0850248504ecd3b3d56356bd5
Daniil <daniil.popoff01@gmail.com>
2023-10-25 10:58:44 +0600

    heart desiase prediction

--- bio_blog/urls.py
+++ bio_blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("about_me/", views.about_me)
+    path("about_me/", views.about_me),
+    path("heart/", views.heart, name="heart")
 ]
